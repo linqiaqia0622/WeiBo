bccd91af27914be6a7fc93d47087f2194ef10b8a
linqiaqia0622 <linqiaqia0622@gmail.com>
2016-08-15 09:26:26 +0800

    lqhmiao

--- miao/src/main/java/com/lqh/miao/MainActivity.java
+++ miao/src/main/java/com/lqh/miao/MainActivity.java
@@ -27,8 +27,11 @@ public class MainActivity extends AppCompatActivity
 
     public String miao;
     //定义数组来存放按钮图片
-    private int mImageViewArray[] = {R.drawable.tabbar_home_auto, R.drawable.tabbar_message_auto,
-            R.drawable.tabbar_discover_auto, R.drawable.tabbar_post_auto};
+    private int mImageViewArray[] = {
+            R.drawable.tabbar_home_auto,
+            R.drawable.tabbar_message_auto,
+            R.drawable.tabbar_discover_auto,
+            R.drawable.tabbar_profile_auto};
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
@@ -39,20 +42,21 @@ public class MainActivity extends AppCompatActivity
 
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+//        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+//        fab.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+//                        .setAction("Action", null).show();
+//            }
+//        });
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
                 this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
         drawer.setDrawerListener(toggle);
         toggle.syncState();
+        mViewPager=(ViewPager)findViewById(R.id.container);
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
@@ -132,28 +136,28 @@ public class MainActivity extends AppCompatActivity
             public void onTabReselected(int position) {
             }
         });
-//        mViewPager.addOnPageChangeListener(new ViewPager.OnPageChangeListener() {
-//            @Override
-//            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
-//
-//            }
-//
-//            @Override
-//            public void onPageSelected(int position) {
-//                bottomNavigationBar.selectTab(position);
-//            }
-//
-//            @Override
-//            public void onPageScrollStateChanged(int state) {
-//
-//            }
-//        });
+        mViewPager.addOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+            @Override
+            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+
+            }
+
+            @Override
+            public void onPageSelected(int position) {
+                bottomNavigationBar.selectTab(position);
+            }
+
+            @Override
+            public void onPageScrollStateChanged(int state) {
+
+            }
+        });
         bottomNavigationBar
-                .setMode(BottomNavigationBar.MODE_CLASSIC);
+                .setMode(BottomNavigationBar.MODE_SHIFTING);
         bottomNavigationBar
                 .setBackgroundStyle(BottomNavigationBar.BACKGROUND_STYLE_RIPPLE);
         bottomNavigationBar
-                .setActiveColor(R.color.colorAccent)
+                .setActiveColor(R.color.colorPrimary)
                 .setInActiveColor("#FFFFFF")
                 .setBarBackgroundColor("#ECECEC");
 
---
+++ miao/src/main/java/com/lqh/miao/ui/fragment/ProfileFragment.java
@@ -0,0 +1,84 @@
+package com.lqh.miao.ui.fragment;
+
+import android.app.Fragment;
+
+import com.lqh.miao.mvp.view.HomeFragmentView;
+import com.lqh.miao.net.entity.Status;
+
+import java.util.ArrayList;
+
+/**
+ * Created by Administrator on 2016/8/12.
+ */
+
+public class ProfileFragment extends Fragment implements HomeFragmentView {
+    @Override
+    public void updateListView(ArrayList<Status> statuselist) {
+
+    }
+
+    @Override
+    public void showLoadingIcon() {
+
+    }
+
+    @Override
+    public void hideLoadingIcon() {
+
+    }
+
+    @Override
+    public void showLoadFooterView() {
+
+    }
+
+    @Override
+    public void hideFooterView() {
+
+    }
+
+    @Override
+    public void showEndFooterView() {
+
+    }
+
+    @Override
+    public void showErrorFooterView() {
+
+    }
+
+    @Override
+    public void setUserName(String userName) {
+
+    }
+
+    @Override
+    public void scrollToTop(boolean refreshData) {
+
+    }
+
+    @Override
+    public void showRecyclerView() {
+
+    }
+
+    @Override
+    public void hideRecyclerView() {
+
+    }
+
+    @Override
+    public void showEmptyBackground() {
+
+    }
+
+    @Override
+    public void hideEmptyBackground() {
+
+    }
+
+    @Override
+    public void popWindowsDestory() {
+
+    }
+}
